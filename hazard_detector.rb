65951225ca94496b6ee73b9683794c8c0fa8ea20
Levin Gerdes <gerdeslevin@gmail.com>
2018-02-06 17:45:40 +0100

    Remove _bb2 suffix from camera and stereo variables in script

--- hazard_detector.rb
+++ hazard_detector.rb
@@ -9,15 +9,15 @@ include Orocos
 
 Bundles.initialize
 
-Bundles.run 'camera_bb2::Task' => 'camera_bb2', 'stereo::Task' => 'stereo', 'hazard_detector::Task' => 'hazard_detector' do
+Bundles.run 'camera_bb2::Task' => 'camera', 'stereo::Task' => 'stereo', 'hazard_detector::Task' => 'hazard_detector' do
 
-    stereo_bb2 = Orocos.name_service.get 'stereo'
-    Orocos.conf.apply(stereo_bb2, ['hdpr_bb2'], :override => true)
-    stereo_bb2.configure
+    stereo = Orocos.name_service.get 'stereo'
+    Orocos.conf.apply(stereo, ['hdpr_bb2'], :override => true)
+    stereo.configure
 
-    camera_bb2 = Orocos.name_service.get 'camera_bb2'
-    Orocos.conf.apply(camera_bb2, ['default'], :override => true)
-    camera_bb2.configure
+    camera = Orocos.name_service.get 'camera'
+    Orocos.conf.apply(camera, ['hdpr_bb2'], :override => true)
+    camera.configure
 
     hazard_detector = Orocos.name_service.get 'hazard_detector'
     Orocos.conf.apply(hazard_detector, ['default'], :override => true)
@@ -31,16 +31,16 @@ Bundles.run 'camera_bb2::Task' => 'camera_bb2', 'stereo::Task' => 'stereo', 'haz
 
     log_replay.use_sample_time = true
 
-    log_replay.camera_firewire_bb2.frame.connect_to camera_bb2.frame_in
-    camera_bb2.left_frame.connect_to                stereo_bb2.left_frame
-    camera_bb2.right_frame.connect_to               stereo_bb2.right_frame
-    stereo_bb2.left_frame_sync.connect_to           hazard_detector.camera_frame
-    stereo_bb2.distance_frame.connect_to            hazard_detector.distance_frame
+    log_replay.camera_firewire_bb2.frame.connect_to camera.frame_in
+    camera.left_frame.connect_to                stereo.left_frame
+    camera.right_frame.connect_to               stereo.right_frame
+    stereo.left_frame_sync.connect_to           hazard_detector.camera_frame
+    stereo.distance_frame.connect_to            hazard_detector.distance_frame
 
-    reader = camera_bb2.right_frame.reader
+    reader = camera.right_frame.reader
 
-    camera_bb2.start
-    stereo_bb2.start
+    camera.start
+    stereo.start
     hazard_detector.start
 
     Vizkit.display hazard_detector.hazard_visualization
